127025fbfe39ea827a6ad79bad1791a7774b9d4e
ryulurala <ryulurala@gmail.com>
2021-01-15 02:02:17 +0900

    Implementation "PlayerController" script - moving the character with animation

---
+++ Ragdoll-test/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    private float positionY = 15.0f;
+    private float positionZ = -15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player");
+        transform.eulerAngles = new Vector3(35.0f, 0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3(target.transform.localPosition.x, positionY, target.transform.localPosition.z + positionZ);
+    }
+}
--- Ragdoll-test/Assets/Scripts/PlayerController.cs
+++ Ragdoll-test/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float playerSpeed = 1.0f;
+    private float walkSpeed = 3.0f;
+    private float runSpeed = 6.0f;
+
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -20,32 +23,50 @@ public class PlayerController : MonoBehaviour
     private void move()
     {
         Vector3 directionVector3 = Vector3.zero;    // 방향 벡터
+        float moveSpeed = 0.0f;
 
         if (Input.GetKey(KeyCode.UpArrow))      // ↑
         {
             directionVector3 += Vector3.forward;
+            moveSpeed = walkSpeed;
         }
         if (Input.GetKey(KeyCode.DownArrow))    // ↓
         {
             directionVector3 += Vector3.back;
+            moveSpeed = walkSpeed;
         }
         if (Input.GetKey(KeyCode.RightArrow))   // →
         {
             directionVector3 += Vector3.right;
+            moveSpeed = walkSpeed;
         }
         if (Input.GetKey(KeyCode.LeftArrow))    // ←
         {
             directionVector3 += Vector3.left;
+            moveSpeed = walkSpeed;
         }
 
-        if (directionVector3.magnitude != 0)
+        if (moveSpeed != 0.0f)      // 움직인다면
         {
-            // 방향을 바라보게 함. (by using. 벡터 정규화)
-            transform.rotation = Quaternion.LookRotation(directionVector3.normalized);
+            if (directionVector3 != Vector3.zero)
+            {
+                // 방향을 바라보게 함. (by using. 벡터 정규화)
+                transform.rotation = Quaternion.LookRotation(directionVector3.normalized);
+            }
 
-            // 캐릭터는 앞으로만 가도록 한다.
-            // 방향 벡터의 크기(magnitude)만큼 움직인다.
-            transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.E))
+            {
+                // 뛰기
+                moveSpeed = runSpeed;
+            }
         }
+
+        // Animation 제어
+        animator.SetBool("IsMoving", moveSpeed != 0);   // 움직임
+        animator.SetBool("IsRunning", moveSpeed == runSpeed);   // 뛰기
+
+        // 캐릭터는 앞으로만 가도록 한다.
+        // 방향 벡터의 크기(magnitude)만큼 움직인다.
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
     }
 }
